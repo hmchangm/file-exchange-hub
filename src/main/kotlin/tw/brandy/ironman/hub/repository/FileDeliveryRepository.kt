50633394fa56a6c478c3c9b1ee45cbf96efccd14
Brandy Chang <dickgg@gmail.com>
2026-05-03 19:30:56 +0800

    refactor: rewrite FileDeliveryRepository with reactive SQL and INSERT IGNORE

--- src/main/kotlin/tw/brandy/ironman/hub/repository/FileDeliveryRepository.kt
+++ src/main/kotlin/tw/brandy/ironman/hub/repository/FileDeliveryRepository.kt
@@ -1,14 +1,20 @@
 package tw.brandy.ironman.hub.repository
 
-import io.quarkus.hibernate.orm.panache.kotlin.PanacheRepositoryBase
+import io.smallrye.mutiny.Uni
+import io.vertx.mutiny.mariadbclient.MariaDBPool
+import io.vertx.mutiny.sqlclient.Tuple
 import jakarta.enterprise.context.ApplicationScoped
 import tw.brandy.ironman.hub.domain.FileDelivery
+import java.time.ZoneOffset
 
 @ApplicationScoped
-class FileDeliveryRepository : PanacheRepositoryBase<FileDelivery, String> {
+class FileDeliveryRepository(private val pool: MariaDBPool) {
 
-    fun findByFileIdAndConsumerId(fileId: String, consumerId: String): FileDelivery? =
-        find("fileId = :fileId and consumerId = :consumerId",
-            mapOf("fileId" to fileId, "consumerId" to consumerId))
-            .firstResult()
+    fun insertIgnore(delivery: FileDelivery): Uni<Unit> =
+        pool.preparedQuery(
+            "INSERT IGNORE INTO file_delivery (id, file_id, consumer_id, note, processed_at) VALUES (?, ?, ?, ?, ?)"
+        ).execute(Tuple.of(
+            delivery.id, delivery.fileId, delivery.consumerId, delivery.note,
+            delivery.processedAt.atOffset(ZoneOffset.UTC).toLocalDateTime()
+        )).map { Unit }
 }
